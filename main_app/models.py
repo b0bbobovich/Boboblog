8f4bf03b4bfecaf7ed22596cd835de38ff297d37
b0bbobovich <91199600+b0bbobovich@users.noreply.github.com>
2022-06-22 00:01:35 +0300

    user profile under the post added

--- main_app/models.py
+++ main_app/models.py
@@ -33,3 +33,17 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('home')
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pics = models.ImageField(null=True, blank=True, upload_to='images/profile/')
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    linkedin_url = models.CharField(max_length=255, null=True, blank=True)
+
+
+
+    def __str__(self):
+        return str(self.user)
